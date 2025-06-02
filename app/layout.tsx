e9e023d845faf16d1b7339558e3f3b43807d0b39
javiers78889 <157335485+javiers78889@users.noreply.github.com>
2025-06-02 10:42:58 -0500

    ajustes

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,14 +7,16 @@ import { ToastContainer } from 'react-toastify'
 import Footer from "@/components/Footer"
 import { ThemeProvider } from "next-themes"
 import Header from "@/components/Header"
+import Head from "next/head"
 
 
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: "Minimal Creative Agency",
-  description: "Apple-inspired design portfolio",
+  title: "MIIC-PANAMÁ",
+  description: "PROYECTO MIIC-PANAMÁ",
+
 }
 
 type TChild = {
@@ -27,9 +29,15 @@ export default function RootLayout({
 
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/favicon-32x32.png" sizes="32x32" />
+        <link rel="icon" href="/favicon-16x16.png" sizes="16x16" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <body className={`${inter.className} min-h-screen bg-background text-foreground`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <ToastContainer />
+          <ToastContainer />
           <div className=" sticky top-0 z-50 border-b-1 ">
             <Header />
           </div>
