2739adf60061627c4c6ba436f5aba8d402ee46b0
javiers78889 <157335485+javiers78889@users.noreply.github.com>
2025-06-02 09:22:32 -0500

    ajustados

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -126,7 +126,7 @@ export default function Timeline() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center mb-12">
           <h2 className="text-3xl font-bold dark:text-white text-gray-900 sm:text-4xl">Nuestra Trayectoria</h2>
-          <p className="mt-4 text-lg dark:text-white text-gray-600 ">La evolución de nuestra agencia a través de los años</p>
+          <p className="mt-4 text-lg dark:text-white text-gray-600 ">La evolución del proyecto MIIC a través de los años</p>
         </div>
 
         <div className="relative">
