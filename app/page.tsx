88615d23ef20772c15dea39a50841fc2af701bbc
javiers78889 <javiers78889@gmail.com>
2025-06-10 15:33:17 -0500

    ajustadoasod

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import FeatureCarousel from "@/components/FeatureCarousel";
 import Hero from "@/components/Hero";
 import Marquee from "@/components/Marquee";
 import Timeline from "@/components/Timeline";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 
 
 export default function Home() {
