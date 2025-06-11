8f19e3fb3cc5137baaf5bce732d2f73b56e9f640
javiers78889 <javiers78889@gmail.com>
2025-06-11 15:20:19 -0500

    asdjasd

--- app/auth/new-password/page.tsx
+++
@@ -1,8 +0,0 @@
-import FormularioNewPwd from '@/components/auth/login/formulario/FormularioNewPwd'
-import React from 'react'
-
-export default function Page() {
-    return (
-        <FormularioNewPwd />
-    )
-}
