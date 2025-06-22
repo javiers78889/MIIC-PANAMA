b1f8c9f7a7221cd1170c1b16c46a4b2ade624029
javiers78889 <javiers78889@gmail.com>
2025-06-21 22:46:59 -0500

    ajustado

--- components/miic/SaveDataForm/SaveDataForm.tsx
+++ components/miic/SaveDataForm/SaveDataForm.tsx
@@ -9,17 +9,17 @@ type TResult = {
 export default function SaveDataForm({ state }: TResult) {
     return (
         <div hidden>
-            <Input type="text" name='ppi' value={state["1. Pregunta Principal de Investigación (P.P.I)"]} />
-            <Input type="text" name='og' value={state["2. Objetivo General"]} />
-            <Input type="text" name='tp' value={state["3. Título del Proyecto"]} />
-            <Input type="text" name='hp' value={state["4. Hipótesis"]} />
-            <Input type="text" name='hpn' value={state["5. Hipótesis Nula"]} />
-            <Input type="text" name='ps1' value={state["6. Pregunta Secundaria 1"]} />
-            <Input type="text" name='ps2' value={state["7. Pregunta Secundaria 2"]} />
-            <Input type="text" name='ps3' value={state["8. Pregunta Secundaria 3"]} />
-            <Input type="text" name='oe1' value={state["9. Objetivo Específico 1"]} />
-            <Input type="text" name='oe2' value={state["10. Objetivo Específico 2"]} />
-            <Input type="text" name='oe3' value={state["11. Objetivo Específico 3"]} />
+            <Input type="text" name='ppi' defaultValue={state["1. Pregunta Principal de Investigación (P.P.I)"]} />
+            <Input type="text" name='og' defaultValue={state["2. Objetivo General"]} />
+            <Input type="text" name='tp' defaultValue={state["3. Título del Proyecto"]} />
+            <Input type="text" name='hp' defaultValue={state["4. Hipótesis"]} />
+            <Input type="text" name='hpn' defaultValue={state["5. Hipótesis Nula"]} />
+            <Input type="text" name='ps1' defaultValue={state["6. Pregunta Secundaria 1"]} />
+            <Input type="text" name='ps2' defaultValue={state["7. Pregunta Secundaria 2"]} />
+            <Input type="text" name='ps3' defaultValue={state["8. Pregunta Secundaria 3"]} />
+            <Input type="text" name='oe1' defaultValue={state["9. Objetivo Específico 1"]} />
+            <Input type="text" name='oe2' defaultValue={state["10. Objetivo Específico 2"]} />
+            <Input type="text" name='oe3' defaultValue={state["11. Objetivo Específico 3"]} />
         </div>
     )
 }
