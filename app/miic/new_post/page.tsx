ef84e1f5414cc90a874374e5a15b3655c0c38b0b
javiers78889 <javiers78889@gmail.com>
2025-08-23 19:01:05 -0500

    control de acceso

--- app/miic/new_post/page.tsx
+++ app/miic/new_post/page.tsx
@@ -1,19 +1,38 @@
-"use client"
+'use client'
 
+import { validar } from "@/action/redirect-action"
+import { userOnline } from "@/action/user-online-action"
 import ListaDePost from "@/components/posts/ListaDePost"
 import Posteos from "@/components/posts/Posteos"
-
-
-
+import { useQuery } from "@tanstack/react-query"
+import { useEffect } from "react"
 
 export default function ImageUploadForm() {
-   
-    return (
-        <div className="grid grid-cols-2 justify-center items-center min-h-screen p-4">
+  const { data, isLoading } = useQuery({
+    queryKey: ["validate"],
+    queryFn: userOnline,
+  })
 
-            <Posteos/>
-            <ListaDePost/>
-          
-        </div>
+  // Redirección solo en useEffect, después de que data esté disponible
+  useEffect(() => {
+    if (data && data.success[0].role !== "admin") {
+      validar() // tu función debe usar router.push() internamente
+    }
+  }, [data])
+
+  // Mientras carga o redirige, muestra loading
+  if (isLoading || (data && data.success[0].role !== "admin")) {
+    return (
+      <div className="flex text-center items-center justify-center">
+        <h2>Cargando...</h2>
+      </div>
     )
+  }
+
+  return (
+    <div className="grid grid-cols-2 justify-center items-center min-h-screen p-4">
+      <Posteos />
+      <ListaDePost />
+    </div>
+  )
 }
